8cb6c2947b6b613c5614d9d617a92e537656235d
smwasifzamanomee <wasifbdjsr@gmail.com>
2023-05-19 14:27:39 +0600

    setup project

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif', 'ui-sans-serif', 'system-ui'],
+        serif: ['ui-serif', 'Georgia'],
+        mono: ['ui-monospace', 'SFMono-Regular'],
+        display: ['Oswald'],
+        body: ['Open Sans'],
+      },
+      screens: {
+        'sm': {'min': '350px','max': '600px'},
+        'md': {'min': '601px', 'max': '820px'},
+        'lg': {'min': '821px', 'max': '1249px' },
+        'xl': { 'min': '1250px'},
+      }
+    },
+  },
+  plugins: [
+    
+  ],
+}
